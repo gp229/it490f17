b8a85cef2f3886d68d9ded0c8710bd351a474efc
gp229 <gp229@njit.edu>
2017-10-10 00:27:40 -0400

    Central Logging working

--- Backend/logProc.php
+++ Backend/logProc.php
@@ -5,10 +5,10 @@ require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 require_once('logger.php.inc');
 
-function doLog($logfile,$level,$machine,$message)
+function doLog($logfile,$level,$machine,$ip,$message)
 {
 	$logConn = new logger($logfile);
-	$logConn->writeLog($level,$machine,$message);
+	$logConn->writeLog($level,$machine,$ip,$message);
 }
 
 function requestProcessor($request)
@@ -22,11 +22,11 @@ function requestProcessor($request)
    switch ($request['type'])
   {
     case "log":
-	if(empty($request['logfile']) || empty($request['level']) || empty($request['machine']) || empty($request['message']))
+	if(empty($request['logfile']) || empty($request['level']) || empty($request['machine']) || empty($request['ip']) || empty($request['message']))
 	{
-		echo "Logfile, level, machine, or message not set for log.".PHP_EOL;
+		echo "Logfile, level, machine, ip, or message not set for log.".PHP_EOL;
 	}
-	doLog($request['logfile'],$request['level'],$request['machine'],$request['message']);
+	doLog($request['logfile'],$request['level'],$request['machine'],$request['ip'],$request['message']);
   }
   return array("returnCode" => '0', 'message'=>"Server received request and processed");
 }
