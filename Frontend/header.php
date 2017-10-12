1f0507ff6e59fa56841d57b912b3ed30926ecb65
gp229 <gp229@njit.edu>
2017-10-11 23:22:52 -0400

    Logout working

--- Frontend/header.php
+++ Frontend/header.php
@@ -9,3 +9,43 @@ if(!isset($_SESSION['loginUser']))
 }
 echo "Hello, ".$_SESSION['loginUser'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="favicon.ico">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div id = "output">
+	status<p>
+    </div>
+    <div class="container">
+
+      <form class="form-signin">
+        <button class="btn btn-lg btn-primary btn-block" type="button" onclick="location.href = 'logout.php';">Logout</button>
+      </form>
+
+    </div> <!-- /container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+</body>
+</html>
--- Frontend/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$_SESSION['dog'] = "jimmy";
-echo $_SESSION['id'];
-var_dump($_SESSION);
-echo session_id();
-?>
