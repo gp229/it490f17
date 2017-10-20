217c4580858b9d652f7ecf22f6b7f3b368f898b0
gp229 <gp229@njit.edu>
2017-10-20 12:39:26 -0400

    Changed key for stockInfo

--- Backend/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-$joe['stocks']['goog'] = array("kappa" => "ok", "fasdf" => "23");
-$joe['stocks']['amz'] = array("kappa" => "ok", "fasdf" => "23");
-var_dump($joe);
-foreach( $joe['stocks']['goog'] as $key => $value)
-{
-	echo $key.$value;
-}
-?>
